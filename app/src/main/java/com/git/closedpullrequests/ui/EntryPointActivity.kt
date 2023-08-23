87b77f53cc017cdcd00068b6375524d5e208d097
Qkprahlad101 <k.prahlad101@gmail.com>
2023-08-23 17:35:22 +0530

    EntryPointActivity updated.

--- app/src/main/java/com/git/closedpullrequests/ui/EntryPointActivity.kt
+++ app/src/main/java/com/git/closedpullrequests/ui/EntryPointActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,33 +17,47 @@ import dagger.hilt.android.AndroidEntryPoint
 class EntryPointActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: ClosedPullRequestsViewModel by viewModels()
+    var adapter = PullRequestAdapter(emptyList())
 
+    private lateinit var recycler: RecyclerView
+    private lateinit var fetchButton: Button
+    private lateinit var ownerEditText: EditText
+    private lateinit var repoEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
-        val fetchButton = findViewById<Button>(R.id.fetchButton)
-        val ownerEditText = findViewById<EditText>(R.id.ownerEditText)
-        val repoEditText = findViewById<EditText>(R.id.repoEditText)
-        var adapter = PullRequestAdapter(emptyList())
-        val spacing = resources.getDimensionPixelSize(R.dimen.item_spacing)
+        recycler = findViewById(R.id.recyclerView)
+        fetchButton = findViewById(R.id.fetchButton)
+        ownerEditText = findViewById(R.id.ownerEditText)
+        repoEditText = findViewById(R.id.repoEditText)
+
         recycler.apply {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
             this.adapter = adapter
         }
 
+
         fetchButton.setOnClickListener {
             val owner = ownerEditText.text.toString().trimEnd()
             val repo = repoEditText.text.toString().trimEnd()
-            viewModel.fetchClosedPullRequests(owner, repo)
+
+            if (owner.isEmpty() || repo.isEmpty()) {
+                Toast.makeText(this, "Please enter both fields", Toast.LENGTH_SHORT).show()
+            } else {
+                viewModel.fetchClosedPullRequests(owner, repo)
+            }
         }
 
-        viewModel.fetchClosedPullRequests("Qkprahlad101", "GithubClosedPullRequests")
+//        viewModel.fetchClosedPullRequests("Qkprahlad101", "GithubClosedPullRequests") //initial data
+        observer()
+    }
 
+    private fun observer() {
+        val spacing = resources.getDimensionPixelSize(R.dimen.item_spacing)
         viewModel.closedPullRequests.observe(this) { closedPullRequests ->
             adapter = PullRequestAdapter(closedPullRequests)
             adapter.setClosedPullRequests(closedPullRequests)
@@ -51,4 +66,5 @@ class EntryPointActivity : AppCompatActivity() {
             recycler.addItemDecoration(ItemSpacingDecoration(spacing))
         }
     }
+
 }
